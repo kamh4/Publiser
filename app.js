908341d2719d08a45aeba6bca324595c85dcbb24
kamh4 <pirmanalsanjury@gmail.com>
2025-01-03 16:46:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  let filePath = './index.html';
+
+  if (req.url === '/logo.png') {
+    filePath = './logo.png';
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Logo not found');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'image/png' });
+        res.end(data);
+      }
+    });
+    return;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Page not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
